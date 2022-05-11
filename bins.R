3290b4bd910cb004e838102eedf86eebe7110ecd
Sudarshan <sudarshanshetty9@gmail.com>
2022-05-11 15:37:25 +0200

    update

---
+++ bins.R
@@ -0,0 +1,6 @@
+First paper is out now in Scientific Reports!    
+  ![Recovery dynamics of the nasopharyngeal microbiota during influenza-like illness in the aging population from Scientific Reports](https://media.springernature.com/lw685/springer-static/image/art%3A10.1038%2Fs41598-022-05618-9/MediaObjects/41598_2022_5618_Fig1_HTML.png?as=webp){ width=25% }  
+The proof-of-concept study is now published in [npj Biofilms Microbiomes](https://doi.org/10.1038/s41522-022-00275-2).   
+
+![An Overview of Design of a diet-based minimal microbiome from npj Biofilms Microbiomes](https://media.springernature.com/lw685/springer-static/image/art%3A10.1038%2Fs41522-022-00275-2/MediaObjects/41522_2022_275_Fig1_HTML.png?as=webp){ width=25% }  
+
